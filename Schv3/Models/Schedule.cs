bd29ad520bda70781875505884d0c2a4a60fab69
Flamichka <72939347+Flamichka@users.noreply.github.com>
2022-11-29 08:54:24 +0300

    Back_Premerge_front

---
+++ Schv3/Models/Schedule.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schv3.Models
+{
+    public class Schedule
+    {
+        [Key][Required] public int Id { get; set; }
+        [Required] public DateOnly date { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_1Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_2Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_3Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_4Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_5Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_6Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_7Class { get; set; }
+        [ForeignKey(nameof(Class))] public int Id_8Class { get; set; }
+        [Required] public int week { get; set; }
+    }
+}
---
+++ Schv3/Models/Subject.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schv3.Models
+{
+    public class Subject
+    {
+        [Key] [Required] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public int Hours { get; set; }
+        [ForeignKey(nameof(Teacher))][Required] public int Id_teacher { get; set; }
+    }
+}
